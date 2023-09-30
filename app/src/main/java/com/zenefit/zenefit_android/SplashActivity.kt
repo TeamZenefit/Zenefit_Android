f51cffdb1b804a9ba5124f127480ef641b79e79a
ChunBaee <mljung10@naver.com>
2023-10-01 00:36:22 +0900

    #1 refactor: Splash Activity를 DataBinding으로 변환

--- app/src/main/java/com/zenefit/zenefit_android/SplashActivity.kt
+++ app/src/main/java/com/zenefit/zenefit_android/SplashActivity.kt
@@ -2,10 +2,13 @@ package com.zenefit.zenefit_android
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.zenefit.zenefit_android.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
+    private lateinit var binding : ActivitySplashBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
